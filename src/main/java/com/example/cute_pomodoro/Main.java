8d4f2377295bfcae705ece0b107d696cb347a0ef
Dieguito-py <diegoBernardi@protonmail.com>
2024-05-19 10:46:39 -0300

    first commit

--- src/main/java/com/example/cute_pomodoro/Main.java
+++ src/main/java/com/example/cute_pomodoro/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
     public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("pomodoro.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+//        stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
